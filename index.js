408bc1baaab4bda3b1274386fff27184cd6ee5e4
Matthew Clark <afromatt6288@hotmail.com>
2023-02-04 14:48:17 -0700

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+
+function superbowlWin(record) {
+    const result = record.find(function (record) {
+        return record.result === "W"
+    })
+    if (!!result === true) {
+        return result.year
+    }  else if (!!result !== true) {
+        return undefined
+    }
+}
+
+
